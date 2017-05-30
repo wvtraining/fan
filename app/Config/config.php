dbaddee6c3b7abc9e41b6bb145b074458620f68d
Rakesh Sahu <rakesh.sahu@webvirtue.com>
2017-05-30 15:00:12 +0530

    local config

---
+++ app/Config/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rakesh Sahu
+ * Date: 5/30/2017
+ * Time: 2:51 PM
+ */
+namespace app\Config;
+$dbconfig = array('host'=>'192.168.2.100','user'=>'global','pass'=>'global','db'=>'fan');
+define("DBCONFIG", $dbconfig);
